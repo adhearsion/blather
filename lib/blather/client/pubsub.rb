f421fc33905e4a8762f2026b1428b20fbcd3a4e7
Jeff Smick <sprsquish@gmail.com>
2009-05-03 00:50:23 -0700

    DRY up the pubsub DSL implementation

--- lib/blather/client/pubsub.rb
+++ lib/blather/client/pubsub.rb
@@ -4,28 +4,31 @@ class Client
   class PubSub
     attr_accessor :host
 
-    def nodes(path, &block)
-      DSL.client.write_with_handler Stanza::DiscoItems.new(:get, node), &block
-    end
-
-    def node(path, &block)
-      if block_given?
-        Node.new(path).info &block
-      else
-        Node.new(path)
-      end
-    end
-
     def affiliations(&callback)
-      DSL.client.write_with_handler Stanza::PubSub.affiliations, &callback
+      request Stanza::PubSub.affiliations(@host), :affiliates, callback
     end
 
     def subscriptions(&callback)
-      DSL.client.write_with_handler Stanza::PubSub.subscriptions, &callback
+      request Stanza::PubSub.subscriptions(@host), :subscriptions, callback
     end
 
+    def nodes(path, &callback)
+      stanza = Stanza::DiscoItems.new(:get, path)
+      stanza.to = @host
+      request stanza, :items, callback
+    end
+
+    def node(path)
+      stanza = Stanza::DiscoInfo.new(:get, path)
+      stanza.to = @host
+      request(stanza) { |node| yield Stanza::PubSub::Node.import(node) }
+    end
+
+    def items(path, list = [], max = nil, &callback)
+      request Stanza::PubSub.items(@host, path, list, max), :items, callback
+    end
+=begin
     def create(node)
-      
     end
 
     def publish(node, payload)
@@ -37,6 +40,12 @@ class Client
 
     def unsubscribe(node)
       DSL.client.write Stanza::PubSub::Unsubscribe.new(:set, host, node, DSL.client.jid)
+    end
+=end
+  private
+    def request(node, method = nil, callback, &block)
+      block = lambda { |node| callback.call node.__send__(method) } unless block_given?
+      DSL.client.write_with_handler(node, &block)
     end
   end
 
