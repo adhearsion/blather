7e86bdf8b332bc64a049a93c4500efbdc33bc246
Jeff Smick <sprsquish@gmail.com>
2009-05-14 10:11:17 -0700

    add single process ping-pong example

---
+++ examples/ping_pong.rb
@@ -0,0 +1,37 @@
+require 'blather/client/dsl'
+$stdout.sync = true
+
+module Ping
+  extend Blather::DSL
+  def self.run; client.run; end
+
+  setup 'echo@jabber.local/ping', 'echo'
+
+  status :from => Blather::JID.new('echo@jabber.local/pong') do |s|
+    puts "serve!"
+    say s.from, 'ping'
+  end
+
+  message :chat?, :body => 'pong' do |m|
+    puts "ping!"
+    say m.from, 'ping'
+  end
+end
+
+module Pong
+  extend Blather::DSL
+  def self.run; client.run; end
+
+  setup 'echo@jabber.local/pong', 'echo'
+  message :chat?, :body => 'ping' do |m|
+    puts "pong!"
+    say m.from, 'pong'
+  end
+end
+
+trap(:INT) { EM.stop }
+trap(:TERM) { EM.stop }
+EM.run do
+  Ping.run
+  Pong.run
+end
