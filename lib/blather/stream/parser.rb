f2443a32fea4baf601f0d98d0bd855f4fe0bd626
Jeff Smick <sprsquish@gmail.com>
2009-05-22 16:16:33 -0700

    fix the parser to work with Nokogiri and add a spec for safety

--- lib/blather/stream/parser.rb
+++ lib/blather/stream/parser.rb
@@ -12,6 +12,8 @@ class Stream # :nodoc:
     def initialize(receiver)
       @receiver = receiver
       @current = nil
+      @namespaces = {}
+      @namespace_definitions = []
       @parser = Nokogiri::XML::SAX::PushParser.new self
     end
 
@@ -19,70 +21,74 @@ class Stream # :nodoc:
       LOG.debug "PARSING: (#{string})" if @@debug
       @stream_error = string =~ /stream:error/
       @parser.write string
-    rescue RuntimeError
-      if $!.message == "Couldn't parse chunk"
-        raise ParseError, $!.message
+    end
+
+    def start_document; end
+    def end_document; end
+    def warning(*args); end
+
+    def start_element_ns(elem, attrs, prefix, uri, namespaces)
+      LOG.debug "START ELEM: (#{[elem, attrs, prefix, uri, namespaces].inspect})" if @@debug
+
+      args = [elem]
+      args << @current.document if @current
+      node = XMPPNode.new *args
+      node.document.root = node unless @current
+
+      attrs.each { |k,v| node[k] = v if k }
+
+      if !@receiver.stopped?
+        @current << node if @current
+        @current = node
+      end
+
+      @namespace_definitions.push []
+      namespaces.each do |pre, href|
+        next if @namespace_definitions.flatten.include?(@namespaces[[pre, href]])
+        ns = node.add_namespace(pre, href)
+        @namespaces[[pre, href]] ||= ns
+      end
+      @namespaces[[prefix, uri]] ||= node.add_namespace(prefix, uri) unless !prefix || namespaces[prefix]
+      node.namespace = @namespaces[[prefix, uri]]
+
+      deliver(node) if elem == 'stream' && !@stream_error
+
+=begin
+      $stderr.puts "\n\n"
+      $stderr.puts [elem, attrs, prefix, uri, namespaces].inspect
+      $stderr.puts @namespaces.inspect
+      $stderr.puts [@namespaces[[prefix, uri]].prefix, @namespaces[[prefix, uri]].href].inspect if @namespaces[[prefix, uri]]
+      $stderr.puts node.inspect
+      $stderr.puts node.document.to_s.gsub(/\n\s*/,'')
+=end
+    end
+
+    def end_element_ns(elem, prefix, uri)
+      LOG.debug "END ELEM: #{[elem, prefix, uri].inspect}" if @@debug
+
+      if elem == 'stream'
+        deliver XMPPNode.new('stream:end')
+      elsif @current.parent != @current.document
+        @namespace_definitions.pop
+        @current = @current.parent
       else
-        raise
-      end
-    end
-
-    def start_document
-    end
-
-    def start_element(elem, attributes_array)
-      e = XMPPNode.new(elem)
-      key = value = nil
-      attributes_array.each do |item|
-        if key
-          case key
-          when "xmlns"
-            e.namespaces.namespace = XML::Namespace.new(e, nil, item)
-          when /^xmlns:(.*)$/
-            e.namespaces.namespace = XML::Namespace.new(e, $1, item)
-          else
-            e[key] = item
-          end
-          key = nil
-        else
-          key = item
-        end
-      end
-      LOG.debug "START ELEM: (#{elem.inspect}, #{attributes_array.inspect})" if @@debug
-
-      if elem == 'stream:stream' && !@stream_error
-        XML::Document.new.root = e
-        @receiver.receive e
-
-      elsif !@receiver.stopped?
-        @current << e  if @current
-        @current = e
+        deliver @current
       end
     end
 
     def characters(chars = '')
       LOG.debug "CHARS: #{chars}" if @@debug
-      @current << XML::Node.new_text(chars) if @current
+      @current << Nokogiri::XML::Text.new(chars, @current.document) if @current
     end
 
-    def end_element(elem)
-      LOG.debug "END ELEM: #{elem.inspect}" if @@debug
-
-      if !@current && elem == "stream:stream"
-        @receiver.receive XMPPNode.new('stream:end')
-
-      elsif @current.parent?
-        @current = @current.parent
-
-      else
-        c, @current = @current, nil
-        XML::Document.new.root = c
-        @receiver.receive c
-
-      end
+    def error(msg)
+      raise ParseError.new(msg)
     end
 
-    def end_document
+  private
+    def deliver(node)
+      @current, @namespaces, @namespace_definitions = nil, {}, []
+      @receiver.receive node
     end
   end #Parser
 
