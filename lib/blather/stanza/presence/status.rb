6315c89068687af09f5c09edda949d0a72bf9c56
Jeff Smick <sprsquish@gmail.com>
2009-11-17 08:27:36 -0800

    finish docs and reformatting of Status class

--- lib/blather/stanza/presence/status.rb
+++ lib/blather/stanza/presence/status.rb
@@ -2,65 +2,72 @@ module Blather
 class Stanza
 class Presence
 
-  # = Status Stanza
+  # # Status Stanza
   #
-  # Presence stanzas are used to express an entity's current network availability (offline or online, along with
-  # various sub-states of the latter and optional user-defined descriptive text), and to notify other entities of
-  # that availability.
+  # [RFC 3921 Section 2.2.2 - Presence Child Elements](http://xmpp.org/rfcs/rfc3921.html#rfc.section.2.2.2)
   #
-  # == State Attribute
+  # Presence stanzas are used to express an entity's current network
+  # availability (offline or online, along with various sub-states of the
+  # latter and optional user-defined descriptive text), and to notify other
+  # entities of that availability.
   #
-  # The +state+ attribute determains the availability of the entity and can be one of the following:
+  # ## "State" Attribute
   #
-  # * +:available+ -- The entity or resource is available
-  # * +:away+ -- The entity or resource is temporarily away.
-  # * +:chat+ -- The entity or resource is actively interested in chatting.
-  # * +:dnd+ -- The entity or resource is busy (dnd = "Do Not Disturb").
-  # * +:xa+ -- The entity or resource is away for an extended period (xa = "eXtended Away").
+  # The `state` attribute determains the availability of the entity and can be
+  # one of the following:
+  #
+  # * `:available`  -- The entity or resource is available
+  # * `:away`       -- The entity or resource is temporarily away.
+  # * `:chat`       -- The entity or resource is actively interested in chatting.
+  # * `:dnd`        -- The entity or resource is busy (dnd = "Do Not Disturb").
+  # * `:xa`         -- The entity or resource is away for an extended period
+  #                    (xa = "eXtended Away").
   #
   # Blather provides a helper for each possible state:
   #
-  #   Status#available?
-  #   Status#away?
-  #   Status#chat?
-  #   Status#dnd?
-  #   Status#xa?
+  #     Status#available?
+  #     Status#away?
+  #     Status#chat?
+  #     Status#dnd?
+  #     Status#xa?
   #
-  # Blather treats the +type+ attribute like a normal ruby object attribute providing a getter and setter.
-  # The default +type+ is +available+.
+  # Blather treats the `type` attribute like a normal ruby object attribute
+  # providing a getter and setter. The default `type` is `available`.
   #
-  #   status = Status.new
-  #   status.state              # => :available
-  #   status.available?         # => true
-  #   status.state = :away
-  #   status.away?              # => true
-  #   status.available?         # => false
-  #   status
-  #   status.state = :invalid   # => RuntimeError
+  #     status = Status.new
+  #     status.state              # => :available
+  #     status.available?         # => true
+  #     status.state = :away
+  #     status.away?              # => true
+  #     status.available?         # => false
+  #     status
+  #     status.state = :invalid   # => RuntimeError
   #
-  # == Type Attribute
+  # ## "Type" Attribute
   #
-  # The +type+ attribute is inherited from Presence, but limits the value to either +nil+ or +:unavailable+
-  # as these are the only types that relate to Status.
+  # The `type` attribute is inherited from Presence, but limits the value to
+  # either `nil` or `:unavailable` as these are the only types that relate to
+  # Status.
   #
-  # == Priority Attribute
+  # ## "Priority" Attribute
   #
-  # The +priority+ attribute sets the priority of the status for the entity and must be an integer between
-  # -128 and 127.
+  # The `priority` attribute sets the priority of the status for the entity
+  # and must be an integer between -128 and 127.
   #
-  # == Message Attribute
+  # ## "Message" Attribute
   #
-  # The optional +message+ element contains XML character data specifying a natural-language description of
-  # availability status. It is normally used in conjunction with the show element to provide a detailed
-  # description of an availability state (e.g., "In a meeting").
+  # The optional `message` element contains XML character data specifying a
+  # natural-language description of availability status. It is normally used
+  # in conjunction with the show element to provide a detailed description of
+  # an availability state (e.g., "In a meeting").
   #
-  # Blather treats the +message+ attribute like a normal ruby object attribute providing a getter and setter.
-  # The default +message+ is nil.
+  # Blather treats the `message` attribute like a normal ruby object attribute
+  # providing a getter and setter. The default `message` is nil.
   #
-  #   status = Status.new
-  #   status.message            # => nil
-  #   status.message = "gone!"
-  #   status.message            # => "gone!"
+  #     status = Status.new
+  #     status.message            # => nil
+  #     status.message = "gone!"
+  #     status.message            # => "gone!"
   #
   # @handler :status
   class Status < Presence
@@ -121,7 +128,9 @@ class Presence
     #
     # @param [<:unavailable, nil>] type the type
     def type=(type)
-      raise ArgumentError, "Invalid type (#{type}). Must be nil or unavailable" if type && type.to_sym != :unavailable
+      if type && type.to_sym != :unavailable
+        raise ArgumentError, "Invalid type (#{type}). Must be nil or unavailable"
+      end
       super
     end
 
@@ -132,7 +141,9 @@ class Presence
     def state=(state) # :nodoc:
       state = state.to_sym if state
       state = nil if state == :available
-      raise ArgumentError, "Invalid Status (#{state}), use: #{VALID_STATES*' '}" if state && !VALID_STATES.include?(state)
+      if state && !VALID_STATES.include?(state)
+        raise ArgumentError, "Invalid Status (#{state}), use: #{VALID_STATES*' '}"
+      end
 
       set_content_for :show, state
     end
@@ -151,7 +162,9 @@ class Presence
     #
     # @param [Fixnum<-128...127>] new_priority
     def priority=(new_priority) # :nodoc:
-      raise ArgumentError, 'Priority must be between -128 and +127' if new_priority && !(-128..127).include?(new_priority.to_i)
+      if new_priority && !(-128..127).include?(new_priority.to_i)
+        raise ArgumentError, 'Priority must be between -128 and +127'
+      end
       set_content_for :priority, new_priority
     end
 
