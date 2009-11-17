1ac5d167dd0cd3ab3486c0f1af27d5715c4f81b3
Jeff Smick <sprsquish@gmail.com>
2009-11-17 06:59:07 -0800

    full documentation for JID

--- lib/blather/jid.rb
+++ lib/blather/jid.rb
@@ -1,10 +1,48 @@
 module Blather
 
-  # This is a simple modification of the JID class from XMPP4R
+  # Jabber ID or JID
+  #
+  # See [RFC 3920 Section 3 - Addressing](http://xmpp.org/rfcs/rfc3920.html#addressing)
+  #
+  # An entity is anything that can be considered a network endpoint (i.e., an ID
+  # on the network) and that can communicate using XMPP. All such entities are
+  # uniquely addressable in a form that is consistent with RFC 2396 [URI]. For
+  # historical reasons, the address of an XMPP entity is called a Jabber
+  # Identifier or JID. A valid JID contains a set of ordered elements formed of a
+  # domain identifier, node identifier, and resource identifier.
+  #
+  # The syntax for a JID is defined below using the Augmented Backus-Naur Form as
+  # defined in [ABNF]. (The IPv4address and IPv6address rules are defined in
+  # Appendix B of [IPv6]; the allowable character sequences that conform to the
+  # node rule are defined by the Nodeprep profile of [STRINGPREP] as documented in
+  # Appendix A of this memo; the allowable character sequences that conform to the
+  # resource rule are defined by the Resourceprep profile of [STRINGPREP] as
+  # documented in Appendix B of this memo; and the sub-domain rule makes reference
+  # to the concept of an internationalized domain label as described in [IDNA].)
+  #
+  #     jid             = [ node "@" ] domain [ "/" resource ]
+  #     domain          = fqdn / address-literal
+  #     fqdn            = (sub-domain 1*("." sub-domain))
+  #     sub-domain      = (internationalized domain label)
+  #     address-literal = IPv4address / IPv6address
+  #
+  # All JIDs are based on the foregoing structure. The most common use of this
+  # structure is to identify an instant messaging user, the server to which the
+  # user connects, and the user's connected resource (e.g., a specific client) in
+  # the form of <user@host/resource>. However, node types other than clients are
+  # possible; for example, a specific chat room offered by a multi-user chat
+  # service could be addressed as <room@service> (where "room" is the name of the
+  # chat room and "service" is the hostname of the multi-user chat service) and a
+  # specific occupant of such a room could be addressed as <room@service/nick>
+  # (where "nick" is the occupant's room nickname). Many other JID types are
+  # possible (e.g., <domain/resource> could be a server-side script or service).
+  #
+  # Each allowable portion of a JID (node identifier, domain identifier, and
+  # resource identifier) MUST NOT be more than 1023 bytes in length, resulting in
+  # a maximum total size (including the '@' and '/' separators) of 3071 bytes.
   class JID
     include Comparable
 
-    # @private
     PATTERN = /^(?:([^@]*)@)??([^@\/]*)(?:\/(.*?))?$/.freeze
 
     attr_reader :node,
