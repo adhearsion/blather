77a23860f02b5d5df6915367da609bc5b43d14c4
Jeff Smick <sprsquish@gmail.com>
2009-11-19 10:51:45 -0800

    remove confusing and bad examples

--- examples/drb_client.rb
+++
@@ -1,5 +0,0 @@
-%w[rubygems lib/blather/client drb/drb].each { |r| require r }
-
-setup 'drb_client@jabber.local', 'drb_client'
-
-when_ready { DRb.start_service 'druby://localhost:99843', self }
--- examples/pong.rb
+++
@@ -1,6 +0,0 @@
-require 'blather/client'
-
-setup 'echo@jabber.local/pong', 'echo'
-message :chat?, :body => 'ping' do |m|
-  say m.from, 'pong'
-end
