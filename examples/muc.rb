e2fd79dd4c043e6dd9759f95d27e6851b59772b9
Alex <maccman@gmail.com>
2010-04-04 00:32:42 +0100

    Better variable naming

--- examples/muc.rb
+++ examples/muc.rb
@@ -7,9 +7,9 @@ message :groupchat?, :body do |m|
   puts "New message: #{m.body}"
 end
 
-muc_invite do |m|
+muc_invite do |invite|
   puts "MUC INVITE"
-  $muc = Blather::DSL::MUC.new(client, m.from, "My Nick 2")  
+  $muc = Blather::DSL::MUC.new(client, invite.from, "My Nick 2")
   $muc.join
   $muc.say("Yo yo - where is my yo yo?")
 end
