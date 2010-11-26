66e23d5f1ae4df1852d9693665626aeda346e540
Anton Mironov <ant.mironov@gmail.com>
2010-11-26 23:03:48 +0500

    Revert "Change path to spec_helper in spec/blather/roster_item_spec.rb to fix error when running tests."
    This reverts commit b17ddb0b3c19161c091c7a69ac3bfd5bf2526a96.

--- spec/blather/roster_item_spec.rb
+++ spec/blather/roster_item_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), *%w[.. .. spec_helper])
+require File.join(File.dirname(__FILE__), *%w[.. spec_helper])
 
 describe Blather::RosterItem do
   it 'can be initialized with Blather::JID' do
