86a848e6abfe795f8feb18aa1e17a3afe9b4f96b
Ben Langfeld <ben@langfeld.me>
2012-02-25 18:32:50 +0000

    [BUGFIX] MUC presence nodes (joining) have a form element on creation

--- lib/blather/stanza/presence/muc.rb
+++ lib/blather/stanza/presence/muc.rb
@@ -5,13 +5,19 @@ class Presence
   class MUC < Status
     register :muc_join, :x, "http://jabber.org/protocol/muc"
 
-    private
-      def muc
-        unless muc = find_first('ns:x', :ns => self.class.registered_ns)
-          self << (muc = XMPPNode.new('x', self.document))
-        end
-        muc
+    def self.new(*args)
+      new_node = super
+      new_node.muc
+      new_node
+    end
+
+    def muc
+      unless muc = find_first('ns:x', :ns => self.class.registered_ns)
+        self << (muc = XMPPNode.new('x', self.document))
+        muc.namespace = self.class.registered_ns
       end
+      muc
+    end
   end # MUC
 
 end # Presence
