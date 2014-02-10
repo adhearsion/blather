adhearsion/blather

commit 8327184acc57c20daeaebb975729ff70207eab67

[BUGFIX] Ensure handler methods are available even when including the DSL in Object
Fixes #116

lib/blather/client/dsl.rb
@@ -85,8 +85,11 @@ module Blather
     autoload :PubSub, File.expand_path(File.join(File.dirname(__FILE__), *%w[dsl pubsub]))
 
     def self.append_features(o)
+      # Generate a method for every stanza handler that exists.
       Blather::Stanza.handler_list.each do |handler_name|
-        o.__send__ :remove_method, handler_name if !o.is_a?(Class) && o.method_defined?(handler_name)
+        o.__send__ :define_method, handler_name do |*args, &callback|
+          handle handler_name, *args, &callback
+        end
       end
       super
     end
@@ -306,15 +309,6 @@ module Blather
         client.write r
       end
       client.write client.caps.c
-    end
-
-    # Generate a method for every stanza handler that exists.
-    Blather::Stanza.handler_list.each do |handler_name|
-      module_eval <<-METHOD, __FILE__, __LINE__
-        def #{handler_name}(*args, &callback)
-          handle :#{handler_name}, *args, &callback
-        end
-      METHOD
     end
   end  # DSL
 end  # Blather

spec/blather/client/dsl_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 require 'blather/client/dsl'
 
+include Blather::DSL
+
 describe Blather::DSL do
   before do
     @client = Blather::Client.new
@@ -262,5 +264,20 @@ describe Blather::DSL do
     @client.stubs(:jid).returns jid
     @dsl.pubsub.should be_instance_of Blather::DSL::PubSub
     @dsl.pubsub.host.should == jid.domain
+  end
+
+  context "included in a class which defines methods which collide with handler names" do
+    class CollisionTarget
+      include Blather::DSL
+
+      def presence
+        :ghostly
+      end
+    end
+
+    it "should override the handlers" do
+      target = CollisionTarget.new
+      target.presence.should be :ghostly
+    end
   end
 end

commit cf6851f8d35c1955dd32c51a4f8143fb3cc99cc9

Fix the DSL module-extended API that was broken by 8327184acc57c20daeaebb975729ff70207eab67

lib/blather/client/dsl.rb
@@ -94,6 +94,18 @@ module Blather
       super
     end
 
+    def self.extended(o)
+      # Generate a method for every stanza handler that exists.
+      Blather::Stanza.handler_list.each do |handler_name|
+        module_eval <<-METHOD, __FILE__, __LINE__
+          def #{handler_name}(*args, &callback)
+            handle :#{handler_name}, *args, &callback
+          end
+        METHOD
+      end
+      super
+    end
+
     # The actual client connection
     #
     # @return [Blather::Client]

spec/blather/client/dsl_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 require 'blather/client/dsl'
 
-include Blather::DSL
-
 describe Blather::DSL do
   before do
     @client = Blather::Client.new