be87dd8c7774b2e6ea91010a8e52dfd797f629f5
Colt Carder <ccarder51@gmail.com>
2014-06-24 15:35:45 +0000

    Removed useless vars and added conditional to guard

--- examples/MUC_echo.rb
+++ examples/MUC_echo.rb
@@ -3,20 +3,16 @@ require 'blather/client/dsl'
 module MUC
   extend Blather::DSL
   when_ready do
-    room = 'room_name'
-    nickname = 'NickName'
     puts "Connected ! send messages to #{jid.stripped}."
-    join room, nickname
+    join 'room_name', 'nick_name'
   end
 
-  message :groupchat?, :body do |m|
-    unless m.delay || m.from == jid.stripped
+  message :groupchat?, :body, proc { |m| m.from != jid.stripped }, delay: nil do |m|
       echo = Blather::Stanza::Message.new
       echo.to = room
       echo.body = m.body
       echo.type = 'groupchat'
       client.write echo
-    end
   end
 end
 MUC.setup 'username', 'password'
