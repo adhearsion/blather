f711e84136e36b8fce13ee3f65d3a3cbc2cd07fa
Colt Carder <ccarder51@gmail.com>
2014-06-24 00:55:49 +0000

    Fixed room var typo

--- examples/MUC_echo.rb
+++ examples/MUC_echo.rb
@@ -13,7 +13,7 @@ module MUC
              #puts m.from #uncomment this to obtain your username
              unless m.delay or m.from == 'your_username' #putting your username here prevents responding to yourself
                  echo = Blather::Stanza::Message.new
-                 echo.to = @room
+                 echo.to = room
                  echo.body = m.body
                  echo.type = 'groupchat'
                  client.write echo
