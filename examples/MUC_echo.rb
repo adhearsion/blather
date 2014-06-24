66402c3cd3bb3539b67e1f24be05bd22f31b5fd4
Colt Carder <ccarder51@gmail.com>
2014-06-24 01:27:53 +0000

    Last Try Bot

--- examples/MUC_echo.rb
+++ examples/MUC_echo.rb
@@ -3,21 +3,21 @@ require 'blather/client/dsl'
 module MUC
   extend Blather::DSL
   when_ready do
-  room = 'hipchat_room'
-  nickname = 'NickName'
-  puts "Connected ! send messages to #{jid.stripped}."
-  join room, nickname
+    room = 'hipchat_room'
+    nickname = 'NickName'
+    puts "Connected ! send messages to #{jid.stripped}."
+    join room, nickname
   end
 
-message :groupchat?, :body do |m|
-  unless m.delay or m.from == 'your_username' 
-  echo = Blather::Stanza::Message.new
-  echo.to = room
-  echo.body = m.body
-  echo.type = 'groupchat'
-  client.write echo
+  message :groupchat?, :body do |m|
+    unless m.delay or m.from == 'your_username' 
+      echo = Blather::Stanza::Message.new
+      echo.to = room
+      echo.body = m.body
+      echo.type = 'groupchat'
+      client.write echo
+    end
   end
-end
 end
 
 username = 'chat_username'
